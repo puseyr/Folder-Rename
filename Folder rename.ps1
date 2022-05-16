d9bce761d0d7f0d741ed3ebfb02515e660e344f0
puseyr <pusey.ronald@gmail.com>
2022-05-15 20:33:20 -0400

    adding ps1 script

---
+++ Folder rename.ps1
@@ -0,0 +1,8 @@
+﻿Get-ChildItem -Path *ESX* -Directory | ForEach-Object -Process {Rename-item
+-Path $_.Name -NewName ($_.name -replace "ESX","VMWARE") -Verbose}
+
+<# Change ESX to target words to look for
+    Change VMWARE to what you want it to be
+    Works for both beginning and end of the document
+    Use ' ' for Directories with spaces
+#>
